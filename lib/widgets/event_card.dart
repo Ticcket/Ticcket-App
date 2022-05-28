91f1d693cb8d481f289c25fcebc2f33fc46ca4db
Kareem El-Giushy <kareem.64123@gmail.com>
2022-05-28 07:04:48 +0200

    Add Widget For Event Cards

---
+++ lib/widgets/event_card.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:ticcket/models/event.dart';
+import 'package:sizer/sizer.dart';
+import 'package:ticcket/pages/event_view.dart';
+
+class EventCard extends StatelessWidget {
+  final Event event;
+  const EventCard({
+    Key? key,
+    required this.event,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder:  (context) => EventScreen(event: event))
+        )
+      },
+        child: Ink(
+          padding: const EdgeInsets.all(
+            20,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          width: 100.w,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 60.w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      event.title,
+                      style: TextStyle(
+                        color: Colors.blueGrey[700],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.timelapse,
+                          color: Colors.blue[300],
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "${event.startAt} - ${event.endAt}",
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 4,
+                        horizontal: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue[50],
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Text(
+                        "Complete - 80%",
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 20.w,
+                child: Image.network(event.logo),
+              ),
+            ],
+          ),
+        ),
+      );
+  }
+}
